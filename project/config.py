d79306976413eb4cea01d442d99133344eb5eef2
AY <ay18682251320@gmail.com>
2023-11-15 20:57:25 +0800

    补充项目架构

---
+++ project/config.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+
+@File    :   config.py  
+@Modify Time : 2023/11/15 20:45 
+@Author  :  Allen.Yang  
+@Contact :   MC36514@um.edu.mo        
+@Description  : 此文件用于实现命令行读取参数设定
+
+"""
---
+++ project/utils.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+
+@File    :   utils.py  
+@Modify Time : 2023/11/15 20:44 
+@Author  :  Allen.Yang  
+@Contact :   MC36514@um.edu.mo        
+@Description  : 此文件提供工具函数，比如测试、计算loss/Bleu等
+
+"""
